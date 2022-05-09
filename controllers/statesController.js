d9f88892597dfb616abfff316282309b377ff2f5
Megan <mleavens19@gmail.com>
2022-05-09 16:56:42 -0400

    404 response status

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -4,7 +4,7 @@ const statesJSONData = require('../model/states.json');
 //GET route
 const getAllStates = async (req, res) => {
     const mongoStates = await State.find();
-    if (!mongoStates) return res.status(204).json({'message': 'No states found.'});
+    if (!mongoStates) return res.status(404).json({'message': 'No states found.'});
 
     const contig = req.query?.contig;
 
@@ -28,6 +28,7 @@ const getAllStates = async (req, res) => {
 }
 
 const getOneState = async (req, res) => {
+    if (!req?.params?.code) return res.status(404);
     let myStateCode = req?.params?.code;
     const stateObj = statesJSONData.find(state => state.code === myStateCode);
     return res.json(stateObj);
