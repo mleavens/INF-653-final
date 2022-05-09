aed08ccffeafb90aa09d3f73e90e459daa47ad7f
Megan <mleavens19@gmail.com>
2022-05-09 18:04:25 -0400

    change

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use('/states', require('./routes/api/states'));
 // app.use('/funfacts', require('./routes/api/funFacts'));
 
 app.all('*', (req, res) => {
+    res.status(404);
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')) {
